84ca5579e7947bbf5c870f1771e77bc4a29de9f9
anantbarthwal <anantbarthwal@gmail.com>
2021-01-24 13:37:16 +0530

    added break

--- prime.py
+++ prime.py
@@ -1,12 +1,13 @@
-x=15
+x=int(15)
 flag = 0
 for num in range (2,x + 1):
     if (num == 2):
         print("2 is a prime number")
         continue
-    for num1 in range (2,num):
+    for num1 in range (2,int(num/2)):
         if num%num1 == 0:
-           flag = flag + 1      
+           flag = flag + 1 
+           break     
     if flag == 0:
         print(num,"is a prime number")
     flag=0
