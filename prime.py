192826bd227058975cc71e93fd8b59b0a4586505
anantbarthwal <anantbarthwal@gmail.com>
2021-01-24 13:48:58 +0530

    removed check for 2

--- prime.py
+++ prime.py
@@ -1,9 +1,6 @@
 x=int(15)
 flag = 0
 for num in range (2,x + 1):
-    if (num == 2):
-        print("2 is a prime number")
-        continue
     for num1 in range (2,int(num/2)):
         if num%num1 == 0:
            flag = flag + 1 
